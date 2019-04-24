763d7d4cf76578aa716daec7f6ab8490847f8edf
Mathias Lengler <mathias@lengler.dev>
2019-04-24 23:38:28 +0200

    PoC: send sudoku grid to js

--- crate/src/lib.rs
+++ crate/src/lib.rs
@@ -1,7 +1,11 @@
 use log::debug;
+use serde::{Deserialize, Serialize};
 use sudoku::cell::OptionCell;
 use sudoku::Sudoku;
+use sudoku::transport::TransportSudoku;
 use wasm_bindgen::prelude::*;
+
+// TODO: add typescript to webpack
 
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
@@ -13,16 +17,77 @@ pub fn run() -> Result<(), JsValue> {
 
     append_hello_dom()?;
 
-    // TODO: send to js land
-    let sudoku = Sudoku::<OptionCell>::new(3);
-
-    debug!("Hello!");
-    debug!("{}", sudoku);
-
     Ok(())
 }
 
-fn append_hello_dom()-> Result<(), JsValue> {
+
+#[wasm_bindgen]
+pub fn get_sudoku_controller() -> SudokuController {
+    use std::convert::TryFrom;
+
+    // 11 Star difficulty
+    let sudoku = Sudoku::<OptionCell>::try_from(vec![
+        vec![8, 0, 0, 0, 0, 0, 0, 0, 0],
+        vec![0, 0, 3, 6, 0, 0, 0, 0, 0],
+        vec![0, 7, 0, 0, 9, 0, 2, 0, 0],
+        vec![0, 5, 0, 0, 0, 7, 0, 0, 0],
+        vec![0, 0, 0, 0, 4, 5, 7, 0, 0],
+        vec![0, 0, 0, 1, 0, 0, 0, 3, 0],
+        vec![0, 0, 1, 0, 0, 0, 0, 6, 8],
+        vec![0, 0, 8, 5, 0, 0, 0, 1, 0],
+        vec![0, 9, 0, 0, 0, 0, 4, 0, 0],
+    ]).unwrap();
+
+    SudokuController {
+        sudoku
+    }
+}
+
+#[wasm_bindgen]
+pub struct SudokuController {
+    sudoku: Sudoku<OptionCell>
+}
+
+#[wasm_bindgen]
+impl SudokuController {
+    pub fn say_hello(&self) {
+        debug!("Hello!");
+    }
+
+    // TODO: add type assertion on TS side
+    pub fn get_sudoku(&self) -> JsValue {
+        JsValue::from_serde(&TransportSudoku::from(&self.sudoku)).unwrap()
+    }
+
+    pub fn test_typescript(&self) -> JsValue {
+        JsValue::from_serde(&TypescriptTest::V1 { foo: false }).unwrap()
+    }
+}
+
+use wasm_typescript_definition::TypescriptDefinition;
+
+#[derive(Serialize, TypescriptDefinition)]
+enum TypescriptTest {
+    #[allow(unused)]
+    V1 {
+        #[serde(rename = "Foo")]
+        foo: bool,
+    },
+    #[allow(unused)]
+    V2 {
+        #[serde(rename = "Bar")]
+        bar: i64,
+        #[serde(rename = "Baz")]
+        baz: u64,
+    },
+    #[allow(unused)]
+    V3 {
+        #[serde(rename = "Quux")]
+        quux: String,
+    },
+}
+
+fn append_hello_dom() -> Result<(), JsValue> {
     let window = web_sys::window().expect("should have a Window");
     let document = window.document().expect("should have a Document");
 
