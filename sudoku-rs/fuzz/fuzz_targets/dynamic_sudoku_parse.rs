57ba53560e128c4662a1c950668072185056dc44
Mathias Lengler <mathias@lengler.dev>
2020-01-16 21:24:12 +0100

    add dynamic_sudoku_parse fuzz target

---
+++ sudoku-rs/fuzz/fuzz_targets/dynamic_sudoku_parse.rs
@@ -0,0 +1,15 @@
+#![no_main]
+
+use std::convert::TryInto;
+
+use libfuzzer_sys::fuzz_target;
+
+use sudoku::{error::Result, DynamicSudoku};
+
+fuzz_target!(|data: String| {
+    fuzz(data);
+});
+
+fn fuzz(data: String) {
+    let sudoku: Result<DynamicSudoku> = data.as_str().try_into();
+}
