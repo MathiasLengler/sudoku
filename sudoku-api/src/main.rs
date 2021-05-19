be5b2a873a4c68744f73d5c7ca73520e9b7cfbf8
Mathias Lengler <mathias@lengler.dev>
2021-05-20 01:08:17 +0200

    add rocket hello world and json example

--- sudoku-api/src/main.rs
+++ sudoku-api/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
+
+use rocket::{Build, Rocket};
+use rocket_contrib::json::JsonValue;
+
+#[get("/")]
+fn hello() -> &'static str {
+    "'sudoku-api' up and running!"
+}
+
+#[get("/api")]
+fn get_test() -> JsonValue {
+    json!({"hello": "world"})
+}
+
+#[launch]
+fn rocket() -> Rocket<Build> {
+    rocket::build().mount("/", routes![hello, get_test])
 }
