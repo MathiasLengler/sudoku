0b87ae60adbea6e6b252b4b109995a67e78e4e98
Mathias Lengler <mathias@lengler.dev>
2022-10-21 02:41:25 +0200

    fix values in samples

--- sudoku-rs/src/samples.rs
+++ sudoku-rs/src/samples.rs
@@ -11,7 +11,7 @@ use crate::grid::Grid;
 
 // TODO: rethink API (unwrap, clone for consumer of specific sudoku)
 pub fn base_2() -> Vec<Grid<U2>> {
-    vec![
+    let mut grids = vec![
         vec![
             vec![0, 3, 4, 0],
             vec![4, 0, 0, 2],
@@ -34,7 +34,13 @@ pub fn base_2() -> Vec<Grid<U2>> {
     .into_iter()
     .map(TryInto::<Grid<U2>>::try_into)
     .collect::<Result<Vec<_>>>()
-    .unwrap()
+    .unwrap();
+
+    grids.iter_mut().for_each(|grid| {
+        grid.fix_all_values();
+    });
+
+    grids
 }
 
 pub fn base_2_candidates_coordinates() -> Grid<U2> {
@@ -46,7 +52,7 @@ pub fn base_2_candidates_coordinates() -> Grid<U2> {
 }
 
 pub fn base_3() -> Vec<Grid<U3>> {
-    vec![
+    let mut grids = vec![
         // 11 Star difficulty
         vec![
             vec![8, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -63,7 +69,13 @@ pub fn base_3() -> Vec<Grid<U3>> {
     .into_iter()
     .map(TryInto::<Grid<U3>>::try_into)
     .collect::<Result<Vec<_>>>()
-    .unwrap()
+    .unwrap();
+
+    grids.iter_mut().for_each(|grid| {
+        grid.fix_all_values();
+    });
+
+    grids
 }
 
 pub fn minimal<Base: SudokuBase>() -> Grid<Base> {
