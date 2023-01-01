ad8c68820c5e36e58ddb5ff7f02d652ad797b05a
Mathias Lengler <mathias@lengler.dev>
2023-01-01 21:16:54 +0100

    delete noDefaultSandbox.tsx

--- sudoku-web/src/app/app.tsx
+++ sudoku-web/src/app/app.tsx
@@ -4,23 +4,15 @@ import { MyTheme } from "./myTheme";
 import { RecoilRoot } from "recoil";
 import { SudokuLoader } from "./sudokuLoader";
 import { RecoilDebug } from "./RecoilDebug";
-import { NoDefaultSandbox } from "./noDefaultSandbox";
 
 export const App = () => {
-    const noDefaultSandbox = false;
     return (
         <RecoilRoot>
             <RecoilDebug />
             <MyTheme>
-                {noDefaultSandbox ? (
-                    <Suspense fallback={"NoDefaultSandbox fallback"}>
-                        <NoDefaultSandbox />
-                    </Suspense>
-                ) : (
-                    <Suspense fallback={"App fallback"}>
-                        <SudokuLoader />
-                    </Suspense>
-                )}
+                <Suspense fallback={"App fallback"}>
+                    <SudokuLoader />
+                </Suspense>
             </MyTheme>
         </RecoilRoot>
     );
