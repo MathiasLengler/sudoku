f08b5b3d7acca3d4542796b6b391336e445076f2
Mathias Lengler <mathias@lengler.dev>
2023-03-13 20:55:11 +0100

    update index.ts

--- sudoku-rs/bindings/index.ts
+++ sudoku-rs/bindings/index.ts
@@ -1,6 +1,8 @@
 // `cargo test` generates a binding file for each exported type
 // This file manually re-exports all bindings.
 
+export * from "./Action";
+export * from "./Candidates";
 export * from "./CellView";
 export * from "./DynamicGeneratorSettings";
 export * from "./DynamicStrategy";
@@ -8,5 +10,10 @@ export * from "./GeneratorSettings";
 export * from "./GeneratorTarget";
 export * from "./GridFormat";
 export * from "./Position";
+export * from "./Reason";
+export * from "./TransportAction";
 export * from "./TransportCell";
+export * from "./TransportDeduction";
+export * from "./TransportReason";
 export * from "./TransportSudoku";
+export * from "./Value";
