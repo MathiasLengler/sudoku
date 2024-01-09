13163abafe177ddf1edf0c7b4aacdba48fe447af
Mathias Lengler <mathias@lengler.dev>
2024-01-09 21:58:25 +0100

    move TODO

--- sudoku-rs/src/cell/mod.rs
+++ sudoku-rs/src/cell/mod.rs
@@ -2,3 +2,10 @@ pub use compact::*;
 
 mod compact;
 pub mod dynamic;
+
+// TODO: Cell without fixed state ("SimpleCell")
+//  could be useful for:
+//  - strategic::Solver operating on a grid of SimpleCells
+//    - for solvers, every value is fixed
+//  - more memory-compact 2d array, with fixed state in separate bitfield
+//  - even more compact Grid<Base, Option<Value<Base>>> for backtracking::Solver
