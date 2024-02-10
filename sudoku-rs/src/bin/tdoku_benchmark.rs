40b353f05ac6ce8875abb4e3c8934cd5d851dd27
Mathias Lengler <mathias@lengler.dev>
2024-02-10 18:31:33 +0100

    restructure tdoku_benchmark for profiling

--- sudoku-rs/src/bin/tdoku_benchmark.rs
+++ sudoku-rs/src/bin/tdoku_benchmark.rs
@@ -1,8 +1,12 @@
+use std::path::PathBuf;
 use std::time::Instant;
+
+use indicatif::ProgressIterator;
 
 use sudoku::base::consts::Base3;
 use sudoku::error::Result;
 use sudoku::grid::deserialization::read_grids_from_file;
+use sudoku::grid::Grid;
 use sudoku::solver::backtracking;
 use sudoku::solver::strategic;
 
@@ -16,14 +20,44 @@ fn main() -> Result<()> {
     let solver_selection = SolverSelection::BacktrackingBitset;
 
     println!("Reading grids");
-    let mut grids = read_grids_from_file::<Base3>("./sudoku-rs/tests/res/tdoku/puzzles1_unbiased")?;
+    let mut grids = read_grids_from_file::<Base3>(
+        PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests/res/tdoku/puzzles1_unbiased"),
+    )?;
     println!("Done");
 
     let before = Instant::now();
 
     let mut total_backtrack_count = 0;
 
-    for (i, grid) in grids.iter_mut().enumerate() {
+    work(&mut grids, solver_selection, &mut total_backtrack_count);
+
+    let after = Instant::now();
+    let total_time = after - before;
+
+    dbg!(total_time);
+    dbg!(grids.len());
+
+    println!(
+        "Grids per second: {:.2}",
+        grids.len() as f64 / total_time.as_secs_f64()
+    );
+    println!(
+        "Avg time per grid: {:?}",
+        total_time / u32::try_from(grids.len()).unwrap()
+    );
+
+    dbg!(total_backtrack_count);
+
+    Ok(())
+}
+
+#[inline(never)]
+fn work(
+    grids: &mut [Grid<Base3>],
+    solver_selection: SolverSelection,
+    total_backtrack_count: &mut u64,
+) {
+    for (i, grid) in grids.iter_mut().enumerate().progress() {
         match solver_selection {
             SolverSelection::Strategic => {
                 assert!(strategic::Solver::new(grid).try_solve().unwrap().is_some());
@@ -31,21 +65,8 @@ fn main() -> Result<()> {
             SolverSelection::BacktrackingBitset => {
                 let mut solver = backtracking::Solver::new(grid);
                 assert!(solver.try_solve().is_some());
-                total_backtrack_count += solver.backtrack_count;
+                *total_backtrack_count += solver.backtrack_count;
             }
         }
-
-        if i % 10000 == 0 {
-            println!("{i}");
-        }
     }
-
-    let after = Instant::now();
-    let total_time = after - before;
-
-    dbg!(total_time);
-    dbg!(grids.len() as f64 / total_time.as_secs_f64());
-    dbg!(total_backtrack_count);
-
-    Ok(())
 }
