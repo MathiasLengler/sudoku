d9eebae2d4f40a402ea53656900458910f5b4c1f
Mathias Lengler <mathias@lengler.dev>
2024-03-09 20:03:32 +0100

    fix type export

--- sudoku-web/src/types.ts
+++ sudoku-web/src/types.ts
@@ -1,7 +1,5 @@
 // Re-exports only the types of the wasm module.
 // This ensures only the worker loads the WASM module at runtime.
 
+export type * from "../../sudoku-rs/bindings";
 export type * from "../../sudoku-wasm/pkg";
-import type { DynamicCell } from "../../sudoku-wasm/pkg";
-
-export type DynamicCells = DynamicCell[];
