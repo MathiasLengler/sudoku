71f87a2126813b124e972fe02bb29ca25082bbd9
Mathias Lengler <mathias@lengler.dev>
2024-03-25 03:08:04 +0100

    add toJSON to comlink proxy wrapper

--- sudoku-web/src/spawnWorker.ts
+++ sudoku-web/src/spawnWorker.ts
@@ -24,6 +24,13 @@ function fixupComlinkProxy<T>(comlinkProxy: Comlink.Remote<T>): Comlink.Remote<T
                 if (property === "then") {
                     return undefined;
                 }
+                // Fix error when passing proxy to `JSON.stringify`
+                if (property === "toJSON") {
+                    return () => {
+                        console.warn("JSON.stringify called on Comlink proxy");
+                        return { COMLINK_PROXY_PLACEHOLDER: true };
+                    };
+                }
                 return (comlinkProxy as unknown as Record<string, unknown>)[property];
             },
         },
