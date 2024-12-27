b865c19bc07681dedefad8a764944e40fc3227b7
Mathias Lengler <mathias@lengler.dev>
2024-12-27 23:34:33 +0100

    enable wasm-pack hot reloading with vite

--- justfile
+++ justfile
@@ -2,11 +2,20 @@ set shell := ['pwsh.exe', '-CommandWithArgs']
 set positional-arguments
 
 default:
-    just --list
+    @just --list
 
 test:
     cargo nextest run
 
+pack-dev-watch: (_pack-watch "pack-dev")
+pack-prod-watch: (_pack-watch "pack-prod")
+
+_pack-watch recipe:
+    watchexec -e rs,toml just {{recipe}}
+
+pack-dev: (_pack "--dev")
+pack-prod: (_pack "--release")
+
 [working-directory: 'sudoku-wasm']
-pack:
-    wasm-pack build --target web . -- -Z build-std=panic_abort,std
+_pack wasm-pack-args:
+    wasm-pack build --target web {{wasm-pack-args}} . -- -Z build-std=panic_abort,std
--- sudoku-web/vite.config.ts
+++ sudoku-web/vite.config.ts
@@ -24,6 +24,20 @@ export default defineConfig(({ mode }) => ({
             "Cross-Origin-Embedder-Policy": "require-corp",
             "Cross-Origin-Opener-Policy": "same-origin",
         },
+        watch: {
+            ignored: [
+                // Defer watching of wasm-pack build output.
+                // Background:
+                // wasm-bindgen-rayon imports the wasm package itself from "snippets/".
+                // "wasm-pack build" first updates the package.json, then emits those snippets, then finishes the containing wasm package.
+                // If the vite watcher reloads between those points, the vite reload fails with:
+                //   Failed to resolve import "../../.."
+                // As a workaround, we ignore everything inside the wasm package,
+                // except the top-level entry points (sudoku_wasm.js and similar).
+                "**/sudoku-wasm/pkg/**",
+                "!**/sudoku-wasm/pkg/sudoku_wasm.*",
+            ],
+        },
     },
     worker: {
         format: "es",
