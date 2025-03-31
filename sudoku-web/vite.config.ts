3e2045330ebcfdd7e65e70861414bad177ba4709
Mathias Lengler <mathias@lengler.dev>
2025-03-31 02:41:08 +0200

    add FIXME

--- sudoku-web/vite.config.ts
+++ sudoku-web/vite.config.ts
@@ -62,6 +62,7 @@ export default defineConfig(({ mode }) => ({
                 background_color: "#121212",
             },
             workbox: {
+                // FIXME: https://dev.sudoku.lengler.dev/assets/roboto-flex-latin-wght-normal-n-dP9qr2.woff2
                 globPatterns: ["**/*.{js,wasm,css,html,png,svg,ico}"],
             },
             pwaAssets: {
