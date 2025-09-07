41e253dc58916c6752eba9310e952c2bb28b4155
Mathias Lengler <mathias@lengler.dev>
2025-09-07 18:01:21 +0200

    JotaiDevTools

--- sudoku-web/src/app/app.tsx
+++ sudoku-web/src/app/app.tsx
@@ -1,14 +1,15 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { NotificationsProvider } from "@toolpad/core/useNotifications";
-import { createStore, Provider as JotaiProvider } from "jotai";
+import { Provider as JotaiProvider } from "jotai";
 import { DevTools } from "jotai-devtools";
+import jotaiDevToolsCss from "jotai-devtools/styles.css?inline";
 import { Suspense } from "react";
 import { SwManager } from "./SwManager";
 import { BasicErrorBoundary, ThemeErrorBoundary } from "./components/ErrorFallback";
 import { FullScreenSpinner } from "./components/FullScreenSpinner";
+import { store } from "./state/store";
 import { Sudoku } from "./sudoku";
 import { MyTheme } from "./theme/myTheme";
-import { store } from "./state/store";
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -25,11 +26,20 @@ const queryClient = new QueryClient({
     },
 });
 
+function JotaiDevTools() {
+    return import.meta.env.DEV ? (
+        <>
+            <style>{jotaiDevToolsCss}</style>
+            <DevTools store={store} />
+        </>
+    ) : null;
+}
+
 export function App() {
     return (
         <BasicErrorBoundary>
             <JotaiProvider store={store}>
-                {import.meta.env.DEV && <DevTools store={store} />}
+                <JotaiDevTools />
                 <QueryClientProvider client={queryClient}>
                     <MyTheme>
                         <ThemeErrorBoundary>
