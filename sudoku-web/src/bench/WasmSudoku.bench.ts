5cdb94e1416643b04b5bbb930655166792137338
Mathias Lengler <mathias@lengler.dev>
2025-11-17 00:44:40 +0100

    add vitest bench

---
+++ sudoku-web/src/bench/WasmSudoku.bench.ts
@@ -0,0 +1,7 @@
+import { bench, describe } from "vitest";
+// import { init } from "../app/state/worker/bg/init";
+
+describe("WasmSudoku", async () => {
+    // await init(1);
+    bench.todo("getTransportSudoku", () => {});
+});
