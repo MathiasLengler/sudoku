db8b9e847d728087c0c82f5f0ce67e6be400be6a
Mathias Lengler <mathias@lengler.dev>
2025-11-16 02:39:19 +0100

    fix input actions

--- sudoku-web/src/app/actions/inputActions.ts
+++ sudoku-web/src/app/actions/inputActions.ts
@@ -4,53 +4,44 @@ import { useCallback } from "react";
 
 export function useToggleCandidateMode() {
     return useAtomCallback(
-        useCallback(
-            (get, set) => () => {
-                const input = get(inputState);
-                set(inputState, { ...input, candidateMode: !input.candidateMode });
-            },
-            [],
-        ),
+        useCallback((get, set) => {
+            const input = get(inputState);
+            set(inputState, { ...input, candidateMode: !input.candidateMode });
+        }, []),
     );
 }
 
 export function useToggleStickyMode() {
     return useAtomCallback(
-        useCallback(
-            (get, set) => () => {
-                const input = get(inputState);
-                if (input.stickyMode) {
-                    set(inputState, {
-                        stickyMode: false,
-                        candidateMode: input.candidateMode,
-                        selectedPos: input.previouslySelectedPos,
-                        previouslySelectedValue: input.selectedValue,
-                    });
-                } else {
-                    set(inputState, {
-                        stickyMode: true,
-                        candidateMode: input.candidateMode,
-                        selectedValue: input.previouslySelectedValue,
-                        previouslySelectedPos: input.selectedPos,
-                        stickyChain: undefined,
-                    });
-                }
-            },
-            [],
-        ),
+        useCallback((get, set) => {
+            const input = get(inputState);
+            if (input.stickyMode) {
+                set(inputState, {
+                    stickyMode: false,
+                    candidateMode: input.candidateMode,
+                    selectedPos: input.previouslySelectedPos,
+                    previouslySelectedValue: input.selectedValue,
+                });
+            } else {
+                set(inputState, {
+                    stickyMode: true,
+                    candidateMode: input.candidateMode,
+                    selectedValue: input.previouslySelectedValue,
+                    previouslySelectedPos: input.selectedPos,
+                    stickyChain: undefined,
+                });
+            }
+        }, []),
     );
 }
 
 export function useEndStickyChain() {
     return useAtomCallback(
-        useCallback(
-            (get, set) => () => {
-                const input = get(inputState);
-                if (input.stickyMode && input.stickyChain) {
-                    set(inputState, { ...input, stickyChain: undefined });
-                }
-            },
-            [],
-        ),
+        useCallback((get, set) => {
+            const input = get(inputState);
+            if (input.stickyMode && input.stickyChain) {
+                set(inputState, { ...input, stickyChain: undefined });
+            }
+        }, []),
     );
 }
