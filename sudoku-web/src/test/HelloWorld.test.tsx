2430e2c4fb95d15ee084b860c2622148d775dcbc
Mathias Lengler <mathias@lengler.dev>
2025-11-23 14:44:54 +0100

    add TODO for visual regression testing

--- sudoku-web/src/test/HelloWorld.test.tsx
+++ sudoku-web/src/test/HelloWorld.test.tsx
@@ -2,6 +2,8 @@ import { expect, test, type ExpectPollOptions } from "vitest";
 import { render } from "vitest-browser-react";
 import HelloWorld from "./HelloWorld.tsx";
 
+// TODO: https://vitest.dev/guide/browser/visual-regression-testing.html
+
 const pollConfig = { timeout: 1000 } satisfies ExpectPollOptions;
 
 test("renders name", async () => {
