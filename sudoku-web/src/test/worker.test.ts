087b84e0245f938c76248fc4dbc924916f7ee1c0
Mathias Lengler <mathias@lengler.dev>
2025-12-21 03:33:24 +0100

    fix lint

--- sudoku-web/src/test/worker.test.ts
+++ sudoku-web/src/test/worker.test.ts
@@ -1,13 +1,14 @@
 import { describe } from "vitest";
 
-// TODO: test and bench worker communication
-// Content: Grid, TransportSudoku, CellWorld
-//  each serialized with the most efficient method
-// Channel:
-//  baseline no worker
-//  comlink proxied class return (return structured cloned)
-//  comlink proxied class return (return transferred Uint8Array)
-// Goal: find new architecture: which parts are executed into worker, which parts stay on main thread?
-//  Probably: only heavy computations in worker, light computations on main thread
-//  We will need to manage multiple class instances, transferring the state between main thread and worker depending on the operation.
-describe("worker", async () => {});
+describe("worker", () => {
+    // TODO: test and bench worker communication
+    // Content: Grid, TransportSudoku, CellWorld
+    //  each serialized with the most efficient method
+    // Channel:
+    //  baseline no worker
+    //  comlink proxied class return (return structured cloned)
+    //  comlink proxied class return (return transferred Uint8Array)
+    // Goal: find new architecture: which parts are executed into worker, which parts stay on main thread?
+    //  Probably: only heavy computations in worker, light computations on main thread
+    //  We will need to manage multiple class instances, transferring the state between main thread and worker depending on the operation.
+});
