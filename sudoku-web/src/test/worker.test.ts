5239ba43e0a25cb4a4b11f716f3b147708f87f40
Mathias Lengler <mathias@lengler.dev>
2025-12-24 23:54:59 +0100

    resolve TODO

--- sudoku-web/src/test/worker.test.ts
+++ sudoku-web/src/test/worker.test.ts
@@ -13,17 +13,6 @@ describe("worker communication", async () => {
     const base = 3;
     const seed = 42n;
 
-    // TODO: test and bench worker communication
-    // Content: Grid, TransportSudoku, CellWorld
-    //  each serialized with the most efficient method
-    // Channel:
-    //  baseline no worker
-    //  comlink proxied class return (return structured cloned)
-    //  comlink proxied class return (return transferred Uint8Array)
-    // Goal: find new architecture: which parts are executed into worker, which parts stay on main thread?
-    //  Probably: only heavy computations in worker, light computations on main thread
-    //  We will need to manage multiple class instances, transferring the state between main thread and worker depending on the operation.
-
     describe("WasmSudoku", () => {
         for (const { name, wasmSudoku } of getWasmSudokuSamples(base, seed)) {
             describe(name, () => {
