739a9212a03879412d56968ff5328a5b3058e862
Mathias Lengler <mathias@lengler.dev>
2026-01-31 23:07:18 +0100

    assert type of strategyMapKeySchema and strategySetSchema

--- sudoku-web/src/app/constants.test-d.ts
+++ sudoku-web/src/app/constants.test-d.ts
@@ -1,12 +1,22 @@
 import { expectTypeOf, test } from "vitest";
 import type * as z from "zod";
-import type { GoalOptimization, GridFormatEnum, GridMetric, GridMetricName, StrategyEnum } from "../types";
+import type {
+    GoalOptimization,
+    GridFormatEnum,
+    GridMetric,
+    GridMetricName,
+    StrategyEnum,
+    StrategyMap,
+    StrategySet,
+} from "../types";
 import {
     strategyEnumSchema,
     type goalOptimizationSchema,
     type gridFormatSchema,
     type gridMetricNameSchema,
     type gridMetricSchema,
+    type strategyMapKeySchema,
+    type strategySetSchema,
 } from "./constants";
 
 test("sudoku-rs bindings match zod schema types", () => {
@@ -22,7 +32,7 @@ test("sudoku-rs bindings match zod schema types", () => {
 
     expectTypeOf<z.output<typeof goalOptimizationSchema>>().toEqualTypeOf<GoalOptimization>();
 
-    // TODO: assert:
-    //  strategyMapKeySchema
-    //  strategySetSchema
+    expectTypeOf<z.output<typeof strategyMapKeySchema>>().toEqualTypeOf<keyof StrategyMap<unknown>>();
+
+    expectTypeOf<z.output<typeof strategySetSchema>>().toEqualTypeOf<StrategySet>();
 });
