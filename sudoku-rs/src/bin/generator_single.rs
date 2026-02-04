c874e00b7db3de5ed74784ee7e8d1024c13951a7
Mathias Lengler <mathias@lengler.dev>
2026-02-04 23:41:58 +0100

    cleanup single gen

--- sudoku-rs/src/bin/generator_single.rs
+++ sudoku-rs/src/bin/generator_single.rs
@@ -1,11 +1,11 @@
 use clap::Parser;
 use env_logger::Env;
-use log::info;
+use log::*;
 use std::time::Instant;
 use sudoku::{DynamicSudoku, error::Result, generator::DynamicGeneratorSettings};
 
 fn parse_generator_settings(s: &str) -> Result<DynamicGeneratorSettings> {
-    let settings: DynamicGeneratorSettings = serde_json::from_str(s)?;
+    let settings = serde_json::from_str(s)?;
     Ok(settings)
 }
 
@@ -16,19 +16,16 @@ struct Args {
     /// Name of the person to greet
     #[arg(short, long, value_parser = parse_generator_settings)]
     generator_settings: DynamicGeneratorSettings,
-
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
-    dbg!(&args);
 
     env_logger::Builder::from_env(Env::default().default_filter_or("info,varisat=warn"))
         .format_indent(Some(0))
         .init();
+
+    debug!("{:?}", args);
 
     let before = Instant::now();
 
